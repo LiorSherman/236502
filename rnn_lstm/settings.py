641e01dcecf7c7a84c0e655b9756339810b6ccdb
LiorShermanRS <lior.sherman@intel.com>
2021-11-22 13:24:12 +0200

    RNN - Tal

---
+++ rnn_lstm/settings.py
@@ -0,0 +1,11 @@
+import os
+
+DEFAULT_DATASET = os.path.join('rnn_lstm', 'dataset', 'deutschl', 'test')
+DEFAULT_EXPERIMENTS_DIR = os.path.join('rnn_lstm', 'experiments_results')
+DEFAULT_SEQ_LEN = 64
+DEFAULT_HIDDEN_SIZE = 256
+DEFAULT_BATCH_SIZE = 64
+DEFAULT_LR = 1e-4
+DEFAULT_PROCESSED_DIR = os.path.join('rnn_lstm', 'processed_dataset')
+DEFAULT_TRAINED_MODELS_DIR = os.path.join('rnn_lstm', 'trained_models')
+
