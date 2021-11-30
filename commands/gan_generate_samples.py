07efc7f770e137c5cf64ac13422ef3628a4d6533
LiorShermanRS <lior.sherman@intel.com>
2021-11-30 20:01:48 +0200

    Changed the way models are saved with torch, modified genarating samples functions accordingly
    Added pre trained model
    Fixed function in preprocess
    Modified gan_generate_samples function

--- commands/gan_generate_samples.py
+++ commands/gan_generate_samples.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 from gan.utils import generate_samples
-
+from pathlib import Path
 
 def desc():
     return 'Generating samples from generator'
@@ -10,17 +10,20 @@ def desc():
 def execute():
     parser = argparse.ArgumentParser(description='GAN : Generate Samples', prog=__name__)
     parser.add_argument('input', help='path to the generator.pt file')
-    parser.add_argument('output', help='path of the output dir')
+    parser.add_argument('--output', default=None, help='path of the output dir')
     parser.add_argument('--num', type=int, default=1, help='num of samples to generate')
-    parser.add_argument('--name', default='My Track', help='sample names')
+    parser.add_argument('--name', default='My_Track', help='sample names')
     args = parser.parse_args()
 
     # check for args validity
     if not os.path.exists(args.input):
-        raise ValueError(f"{args.input} is not a valid dataset dir path")
+        raise ValueError(f"{args.input} is not a valid model path")
 
-    if not os.path.exists(args.output):
-        os.makedirs(args.output)
+    out_path = Path(args.input).parent.absolute()
+    if args.output is not None:
+        out_path = args.output
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
 
-    generate_samples(args.input, args.output, num_samples=args.num, sample_name=args.name)
+    generate_samples(args.input, out_path, num_samples=args.num, sample_name=args.name)
 
